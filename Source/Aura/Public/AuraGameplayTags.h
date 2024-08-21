274d0ab557a6e34d5a90d4ca69e7a00d82dab454
Necknibbler <jacoblaughton@gmail.com>
2024-08-20 18:23:14 -0700

    GameplayTags Singleton added. Asset Manager Singelton added.

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,26 @@
+// Copyright Message
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * Aura GameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+
+public:
+    static const FAuraGameplayTags& Get() { return GameplayTags; }
+    static void InitializeNativeGameplayTags();
+
+    FGameplayTag Attributes_Secondary_Armor;
+protected:
+
+private:
+    static FAuraGameplayTags GameplayTags;
+};
